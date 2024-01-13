748f9120e4e7ec3b927eef693ff5777d230d97e3
Gabriel <gabriel.pinheiro1240@gmail.com>
2024-01-13 12:57:09 -0300

    criando projeto

---
+++ src/main/java/com/example/APIspringboot/ApIspringbootApplication.java
@@ -0,0 +1,14 @@
+package com.example.APIspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIspringbootApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/APIspringboot/ApIspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
